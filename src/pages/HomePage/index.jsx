815fe9587570c8a0bf9144a62449e8854b8355e0
GabrielNunex <gabrielnunes.design@gmail.com>
2024-09-12 09:21:05 -0300

    Update index.jsx

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,5 +1,5 @@
 import { AboutMeSection } from "../../components/AboutMeSection/aboutMeSection"
-import { Footer } from "../../components/footer/footer"
+import { Footer } from "../../components/Footer/footer"
 import { Header } from "../../components/header/header"
 import { ProjectsSection } from "../../components/ProjectsSection"
 import { TechSection } from "../../components/TechSection"
